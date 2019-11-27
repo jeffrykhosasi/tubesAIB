9c87695ced5a613e7c072fe7b763b4c5e1e76621
m-vd <mikvadi.9@gmail.com>
2019-11-27 17:55:27 +0700

    working version

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import pandas as pd
 from flask import Flask, jsonify, request, render_template
 import pickle
 import lightgbm
+import sys
 
 # Load model
-model = pickle.load(open('lgb_model.pkl', 'rb'))
+model = pickle.load(open('lgbmc_model.sav', 'rb'))
 
 # App
 app = Flask(__name__)
@@ -16,22 +17,26 @@ def home():
 
 @app.route('/', methods=['POST'])
 def predict():
-    # get data
-    data = request.get_json(force=True)
 
-    # convert data into dataframe
-    data.update((x, [y]) for x, y in data.items())
-    data_df = pd.DataFrame.from_dict(data)
+    # get data from form
+    # ----
+    # needed data is based on the model. the current lgbmc_model.sav used has 3 features: is_4G, n_ad_seen, app_code_count_n, 
+    # thus we ask user to input these 3 attributes and take it from the form.
+    # ----
+    # there is a need to explicitly change the type of the object to create a proper dataframe to be used in the model
+    is_4g = int(request.form['is_4g'])
+    app_code_count_n = int(request.form['app_code_count_n'])
+    n_ad_seen = int(request.form['n_ad_seen'])
 
-    # predictions
-    result = model.predict(data_df)
+    # prepare data as a dataframe for input
+    data = [[is_4g, n_ad_seen, app_code_count_n]]
+    data = pd.DataFrame(data, columns = ['is_4G','n_ad_seen','app_code_count_n'])
 
-    # send back to browser
-    output = {'results': int(result[0])}
+    # predict
+    result = model.predict_proba(data)[:,1]
 
-    # return data
-    return jsonify(results=output)
-
+    # return prediction result
+    return jsonify({'result': result.tolist()}) 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=False)
